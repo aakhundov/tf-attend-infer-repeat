a66fb1ac3c53e8b9d033d7f0c6b4986e896b49bc
Adnan Akhundov <adnan.akhundov@gmail.com>
2017-08-08 19:26:51 +0200

    Variable names and name scopes have been added for better graph-level diagnostics.

--- vae.py
+++ vae.py
@@ -8,26 +8,36 @@ def vae(inputs, input_dim, rec_hidden_units, latent_dim,
     input_size = tf.shape(inputs)[0]
 
     next_layer = inputs
-    for units in rec_hidden_units:
-        next_layer = layers.fully_connected(next_layer, units, activation_fn=activation)
+    for i in range(len(rec_hidden_units)):
+        with tf.name_scope("recognition_" + str(i+1)) as scope:
+            next_layer = layers.fully_connected(
+                next_layer, rec_hidden_units[i], activation_fn=activation, scope=scope
+            )
 
-    recognition_mean = layers.fully_connected(next_layer, latent_dim, activation_fn=None)
-    recognition_log_variance = layers.fully_connected(next_layer, latent_dim, activation_fn=None)
-    standard_normal_sample = tf.random_normal([input_size, latent_dim])
+    with tf.name_scope("rec_mean") as scope:
+        recognition_mean = layers.fully_connected(next_layer, latent_dim, activation_fn=None, scope=scope)
+    with tf.name_scope("rec_log_variance") as scope:
+        recognition_log_variance = layers.fully_connected(next_layer, latent_dim, activation_fn=None, scope=scope)
 
-    recognition_sample = recognition_mean + standard_normal_sample * tf.sqrt(tf.exp(recognition_log_variance))
+    with tf.name_scope("rec_sample"):
+        standard_normal_sample = tf.random_normal([input_size, latent_dim])
+        recognition_sample = recognition_mean + standard_normal_sample * tf.sqrt(tf.exp(recognition_log_variance))
 
     next_layer = recognition_sample
-    for units in gen_hidden_units:
-        next_layer = layers.fully_connected(next_layer, units, activation_fn=activation)
+    for i in range(len(gen_hidden_units)):
+        with tf.name_scope("generative_" + str(i+1)) as scope:
+            next_layer = layers.fully_connected(
+                next_layer, gen_hidden_units[i], activation_fn=activation, scope=scope
+            )
 
-    generative_mean = layers.fully_connected(next_layer, input_dim, activation_fn=None)
-    standard_normal_sample2 = tf.random_normal([input_size, input_dim])
+    with tf.name_scope("gen_mean") as scope:
+        generative_mean = layers.fully_connected(next_layer, input_dim, activation_fn=None, scope=scope)
 
-    generative_sample = generative_mean + standard_normal_sample2 * likelihood_std
-
-    reconstruction = tf.nn.sigmoid(
-        generative_sample
-    )
+    with tf.name_scope("gen_sample"):
+        standard_normal_sample2 = tf.random_normal([input_size, input_dim])
+        generative_sample = generative_mean + standard_normal_sample2 * likelihood_std
+        reconstruction = tf.nn.sigmoid(
+            generative_sample
+        )
 
     return reconstruction, recognition_mean, recognition_log_variance
