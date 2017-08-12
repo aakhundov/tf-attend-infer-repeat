779869beae7b03c60905d136aea7402848c5c6f5
aakhundov <adnan.akhundov@gmail.com>
2017-08-12 15:10:27 +0200

    VAE default activation function has been reverted to tf.nn.softplus.

--- vae.py
+++ vae.py
@@ -3,7 +3,7 @@ import tensorflow.contrib.layers as layers
 
 
 def vae(inputs, input_dim, rec_hidden_units, latent_dim,
-        gen_hidden_units, likelihood_std=0.0, activation=tf.nn.relu):
+        gen_hidden_units, likelihood_std=0.0, activation=tf.nn.softplus):
 
     input_size = tf.shape(inputs)[0]
 
