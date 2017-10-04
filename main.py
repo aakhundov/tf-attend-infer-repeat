8a804005e675c67e838565b15201a1c57b782f0d
Adnan Akhundov <adnan.akhundov@gmail.com>
2017-10-04 17:11:36 +0200

    One-shot annealing of z_pres log-odds prior from -0.01 to -10.0 after 5k iterations has been added. Scale variance prior has been decreased from 0.1 to 0.02 to encourage smaller attention window sizes and discourage attending two digits with one large attention window.

--- main.py
+++ main.py
@@ -93,15 +93,23 @@ for i in range(2):
             model_inputs[i][0], model_inputs[i][1],
             max_steps=3, max_digits=2, rnn_units=256, canvas_size=CANVAS_SIZE, windows_size=28,
             vae_latent_dimensions=50, vae_recognition_units=(512, 256), vae_generative_units=(256, 512),
-            scale_prior_mean=-1.0, scale_prior_variance=0.1, shift_prior_mean=0.0, shift_prior_variance=1.0,
+            scale_prior_mean=-1.0, scale_prior_variance=0.02, shift_prior_mean=0.0, shift_prior_variance=1.0,
             vae_prior_mean=0.0, vae_prior_variance=1.0, vae_likelihood_std=0.3,
             scale_hidden_units=64, shift_hidden_units=64, z_pres_hidden_units=64,
-            z_pres_prior_log_odds=-1.0, z_pres_temperature=1.0, stopping_threshold=0.99,
+            z_pres_prior_log_odds=-0.01, z_pres_temperature=1.0, stopping_threshold=0.99,
             learning_rate=1e-3, gradient_clipping_norm=100.0,
             num_summary_images=NUM_IMAGES_TO_SAVE, train=(i == 0), reuse=(i == 1), scope="air",
             annealing_schedules={
-                "z_pres_prior_log_odds": {"init": -0.2, "min": -10.0, "factor": 1.5, "iters": 1000},
-                "learning_rate": {"init": 1e-3, "min": 1e-4, "factor": 0.5, "iters": 10000}
+                "z_pres_prior_log_odds": {
+                    "init": -0.01, "min": -10.0,
+                    "factor": 1000.0, "iters": 5000,
+                    "staircase": True
+                },
+                "learning_rate": {
+                    "init": 1e-3, "min": 1e-4,
+                    "factor": 0.5, "iters": 10000,
+                    "staircase": True
+                }
             }
         )
     )
