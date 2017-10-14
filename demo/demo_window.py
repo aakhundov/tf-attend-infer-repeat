14d9e8292564e03670bc0293da46933cccf28b67
Adnan Akhundov <adnan.akhundov@gmail.com>
2017-10-14 14:16:13 +0200

    Labels have been added above canvases.

--- demo/demo_window.py
+++ demo/demo_window.py
@@ -20,60 +20,72 @@ class DemoWindow(ttk.Frame):
 
         self._create_controls()
 
-        self.columnconfigure(0, weight=1, minsize=200)
-        self.columnconfigure(1, weight=1, minsize=200)
-        self.columnconfigure(2, weight=0, minsize=100)
-        self.rowconfigure(0, weight=1, minsize=200)
+        self.columnconfigure(0, weight=410, minsize=215)
+        self.columnconfigure(1, weight=410, minsize=210)
+        self.columnconfigure(2, weight=140, minsize=65)
+        self.rowconfigure(0, weight=1, minsize=220)
 
         self.master.after(50, lambda: master.focus_force())
         self.master.after(100, self._reconstruct)
 
     def _create_controls(self):
         self.frm_canvas_orig = ttk.Frame(self, padding=(10, 10, 5, 10))
+        self.lbl_orig = ttk.Label(self.frm_canvas_orig, text="Original image [draw here]:")
         self.cnv_orig = PixelCanvas(
             self.frm_canvas_orig, self.canvas_size, self.canvas_size, drawable=True,
             highlightthickness=0, borderwidth=0, width=400, height=400
         )
 
         self.frm_canvas_orig.grid(row=0, column=0, sticky=(tk.N, tk.S, tk.W, tk.E))
-        self.cnv_orig.grid(row=0, column=0, sticky=(tk.N, tk.S, tk.W, tk.E))
+        self.lbl_orig.grid(row=0, column=0, sticky=(tk.N, tk.S, tk.W, tk.E))
+        self.cnv_orig.grid(row=1, column=0, sticky=(tk.N, tk.S, tk.W, tk.E))
         self.frm_canvas_orig.columnconfigure(0, weight=1, minsize=200)
-        self.frm_canvas_orig.rowconfigure(0, weight=1, minsize=200)
+        self.frm_canvas_orig.rowconfigure(0, weight=0, minsize=20)
+        self.frm_canvas_orig.rowconfigure(1, weight=1, minsize=200)
 
         self.frm_canvas_rec = ttk.Frame(self, padding=(5, 10, 5, 10))
+        self.lbl_rec = ttk.Label(self.frm_canvas_rec, text="Reconstructed image:")
         self.cnv_rec = PixelCanvas(
             self.frm_canvas_rec, self.canvas_size, self.canvas_size, drawable=False,
             highlightthickness=0, borderwidth=0, width=400, height=400
         )
 
         self.frm_canvas_rec.grid(row=0, column=1, sticky=(tk.N, tk.S, tk.W, tk.E))
-        self.cnv_rec.grid(row=0, column=0, sticky=(tk.N, tk.S, tk.W, tk.E))
+        self.lbl_rec.grid(row=0, column=0, sticky=(tk.N, tk.S, tk.W, tk.E))
+        self.cnv_rec.grid(row=1, column=0, sticky=(tk.N, tk.S, tk.W, tk.E))
         self.frm_canvas_rec.columnconfigure(0, weight=1, minsize=200)
-        self.frm_canvas_rec.rowconfigure(0, weight=1, minsize=200)
+        self.frm_canvas_rec.rowconfigure(0, weight=0, minsize=20)
+        self.frm_canvas_rec.rowconfigure(1, weight=1, minsize=200)
 
         self.frm_windows = ttk.Frame(self, padding=(0, 0, 0, 0))
         self.frm_windows.grid(row=0, column=2, sticky=(tk.N, tk.S, tk.W, tk.E))
         self.frm_windows.columnconfigure(0, weight=1)
 
-        self.frm_canvas_win, self.cnv_win = [], []
+        self.frm_canvas_win, self.lbl_win, self.cnv_win = [], [], []
 
         for i in range(3):
             frm_canvas_win = ttk.Frame(
                 self.frm_windows,
-                padding=(5, 10 if i == 0 else 5, 10, 10 if i == 2 else 5)
+                padding=(5, 10 if i == 0 else 0, 10, 10 if i == 2 else 0)
+            )
+            lbl_win = ttk.Label(
+                frm_canvas_win, text="VAE rec. {0}:".format(i+1)
             )
             cnv_win = PixelCanvas(
                 frm_canvas_win, self.window_size, self.window_size, drawable=False,
-                highlightthickness=0, borderwidth=0, width=125, height=125
+                highlightthickness=0, borderwidth=0, width=120, height=120
             )
 
             frm_canvas_win.grid(row=i, column=0, sticky=(tk.N, tk.S, tk.W, tk.E))
-            cnv_win.grid(row=0, column=0, sticky=(tk.N, tk.S, tk.W, tk.E))
-            frm_canvas_win.columnconfigure(0, weight=1, minsize=100)
-            frm_canvas_win.rowconfigure(0, weight=1, minsize=100)
+            lbl_win.grid(row=0, column=0, sticky=(tk.S, tk.W))
+            cnv_win.grid(row=1, column=0, sticky=(tk.N, tk.S, tk.W, tk.E))
+            frm_canvas_win.columnconfigure(0, weight=1, minsize=50)
+            frm_canvas_win.rowconfigure(0, weight=0, minsize=20)
+            frm_canvas_win.rowconfigure(1, weight=1, minsize=50)
 
-            self.frm_windows.rowconfigure(i, weight=1, minsize=100)
+            self.frm_windows.rowconfigure(i, weight=1)
             self.frm_canvas_win.append(frm_canvas_win)
+            self.lbl_win.append(lbl_win)
             self.cnv_win.append(cnv_win)
 
     def _reconstruct(self):
